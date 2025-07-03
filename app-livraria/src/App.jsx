080737d72064e932f5a56f30e11e9d503f686bcd
Klissia Martins <akfma.snf24@uea.edu.br>
2025-07-02 22:58:00 -0400

    seding v2

--- app-livraria/src/App.jsx
+++ app-livraria/src/App.jsx
@@ -5,53 +5,52 @@ import TabelaBody from './components/TabelaBody';
 import TabelaFoot from './components/TabelaFoot';
 
 class App extends Component {
-  //criando estado inicial
   state = {
     livros: []
   };
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("/api/livros.json")
-    .then(response => response.json())
-    .then(livros => this.setState({livros}))
-    .catch(error =>{
-      console.error("Erro na requisição", error);
-    })
-    .finally(function(){
-      console.log("Requisição finalizada");
-    })
+      .then(response => response.json())
+      .then(livros => this.setState({ livros }))
+      .catch(error => {
+        console.error("Erro na requisição", error);
+      })
+      .finally(() => {
+        console.log("Requisição finalizada");
+      });
   }
 
-  //Remover Livro  
-  handleRemoveLine = id =>(
-    this.setState(prevState =>({
-      livros : prevState.livros.filter(i => i.id !== id)
-    }))
-  )
+  handleRemoveLine = (id) => {
+    this.setState(prevState => ({
+      livros: prevState.livros.filter(i => i.id !== id)
+    }));
+  }
 
-  //ordenar crescente
-  handleOrdenarCrescente = () =>(
-    this.setState(prevState =>({
-      livros:[...prevState.livros].sort((a,b)=>a.titulo.localeCompare(b.this))
-    }))
-  )
+  handleOrdenarCrescente = () => {
+    this.setState(prevState => ({
+      livros: [...prevState.livros].sort((a, b) => a.titulo.localeCompare(b.titulo))
+    }));
+  }
 
-  //ordenar decrescente
-  handleOrdenarDecrescente = () =>(
-    this.setState(prevState =>({
-      livros:[...prevState.livros].sort((a,b)=>b.titulo.localeCompare(a.this))
-    }))
-  )
+  handleOrdenarDecrescente = () => {
+    this.setState(prevState => ({
+      livros: [...prevState.livros].sort((a, b) => b.titulo.localeCompare(a.titulo))
+    }));
+  }
 
   render() {
     return (
       <table className='tabela'>
-        <TabelaHead/>
+        <TabelaHead 
+          ordenarCrescente={this.handleOrdenarCrescente}
+          ordenarDecrescente={this.handleOrdenarDecrescente}
+        />
         <TabelaBody 
-          livros = {this.state.livros}
+          livros={this.state.livros}
           removerLinha={this.handleRemoveLine}
         />
-        <TabelaFoot qtdLivros = {this.state.livros.length}/>
+        <TabelaFoot qtdLivros={this.state.livros.length} />
       </table>
     );
   }
--- app-livraria/src/components/TabelaHead.jsx
+++ app-livraria/src/components/TabelaHead.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const TabelaHead = ()=> (
-    <thead>
-        <tr>
-            <th colSpan="4">Tabela de Livros</th>
-        </tr>
-        <tr>
-            
-            <th>Título</th>
-            <th>Autor</th>
-        </tr>
-    </thead>
+const TabelaHead = ({ ordenarCrescente, ordenarDecrescente }) => (
+  <thead>
+    <tr>
+      <th colSpan="4">Tabela de livros</th>
+    </tr>
+    <tr>
+      <th>Título</th>
+      <th>Autor</th>
+      <th>ID</th>
+      <th>
+        <div className="container-setinhas">
+          <button
+            onClick={ordenarCrescente}
+            aria-label="Ordenar crescente"
+            className="botao-setinha"
+          >
+            ↑
+          </button>
+          <button
+            onClick={ordenarDecrescente}
+            aria-label="Ordenar decrescente"
+            className="botao-setinha"
+          >
+            ↓
+          </button>
+        </div>
+      </th>
+    </tr>
+  </thead>
 );
 
 export default TabelaHead;
